b0a99242b5e141c41a65d4f2ca6a2aab22309dcc
chelqo <chelqo@gmail.com>
2023-02-12 21:27:15 -0300

    Add files via upload

--- CV/js/scripts.js
+++ CV/js/scripts.js
@@ -1,23 +1,21 @@
-/*
-    Scripts de JavaScript del CV
- */
+// Scripts de JavaScript del CV
 
 const msg = "Cambiando contenido de IFRAME desde addEventListener"
 
-function changeContents(stringUrl, stringBoton) {
-    document.getElementById('Cuerpo').setAttribute('src', stringUrl)
-    console.log(stringBoton + ' presionado, redirección hacia: ' + stringUrl);
-    console.log(msg);
+function changeContents(stringURL, stringButton) {
+	document.getElementById('Cuerpo').setAttribute('src', stringURL);
+	console.log(stringButton + ' presionado, redirección hacia' + stringURL);
+	console.log(msg);
 }
 
 document.getElementById('Boton1').addEventListener('click', function () {
-    changeContents('html/page1.html', 'Boton1')
+	changeContents('html/page1.html', 'Boton1')
 })
 
 document.getElementById('Boton2').addEventListener('click', function () {
-    changeContents('html/page2.html', 'Boton2')
+	changeContents('html/page2.html', 'Boton2')
 })
 
 document.getElementById('Boton3').addEventListener('click', function () {
-    changeContents('html/page3.html', 'Boton3')
+	changeContents('html/page3.html', 'Boton3')
 })
